c639f94f4e7a1cdffa875ba7b4229aa730488293
Mike Dunton <michael.dunton@patlive.com>
2013-07-07 13:13:49 -0400

    Added function to save files and updated ToDO list

--- README.md
+++ README.md
@@ -1,31 +1,11 @@
-start
-
-	now = date()
-
-	if( currDate.isSet())
-		if(now > currDate)
-			create new folder for saves with now.mmddyyyy as name
-			currDate = now.mmddyyy
-			save image to urrDateFolder with filename now.
-		else
-			save image to currDateFolder with filename now.
-	else
-		if(now.mmddyyyy is a folder in save location) //we started the application on the same day we stopped it 
-			currDate = now
-			save image to currDateFolder with filename now.
-		else
-			currDate = now
-			create new folder currDatefolder for saves
-			save image to currDateFolder with filename now.
-	sleep(numMinWait)
-end
 
 
 
-*TODO*
-Get percentage of change in picture from last picture
-	if change is sanifigant
-		save it. 	
-		increase capture rate
+#TODO
+1. Get percentage of change in picture from last picture
+	*Check if change is sanifigant 	
+	*increase capture rate or maybe video stream
+2. Email On certain events.
+    *Add email to config
 
 
--- pycaptcha.py
+++ pycaptcha.py
@@ -1,32 +1,62 @@
-import ConfigParser, os, time
-from datetime import date
+import ConfigParser, urllib2 ,os, time
+from datetime import date, datetime
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('config.cfg'))
 url = config.get("one", "URL")
 rootPath = config.get("one", "SaveLocation")
 waitTime = config.getint("one", "WaitTime")
+workingDir = -1
 
-print url,rootPath, waitTime
-workingFolder = -1
+def saveImage(theUrl, location):
+    try:
+        openedUrl = urllib2.urlopen(theUrl)
+        streamJpgCapture = open(location + '/' +datetime.now().time().isoformat(), 'w')
+        streamJpgCapture.write(openedUrl.read())
+        streamJpgCapture.close()
+        return True
+    except urllib2.URLError as e:
+        print e
+        return False
 
-#while(true):
-for n in range(0, 2):
+   
+
+while(true):
     today = date.today()
-    if( workingFolder == -1 ):
-        workingFolder = today.isoformat();
-        if not os.path.exists(rootPath + workingFolder):
-            os.makedirs(rootPath + workingFolder)
+    if( workingDir == -1 ):
+        workingDir = today.isoformat();
+        if not os.path.exists(rootPath + workingDir):
+            print'Creating Directory ' + rootPath + workingDir
+            os.makedirs(rootPath + workingDir)
+            print 'Saving the Image to workingDir\n'
+            saveImage(url, workingDir)
+            print 'Sleeping' + str(waitTime) + ' seconds'
+            time.sleep(waitTime)
         else:
-            print 'Todays Directory Exists'
+            print 'Todays Directory Exists\n'
+            print 'Save the image to workingDir\n'
+            saveImage(url, workingDir)
+            print 'Sleeping' + str(waitTime) + ' seconds'
+            time.sleep(waitTime);
     else:
-        if(today.isoformat() > workingFolder):
-            workingFolder = today.isoformat();
-            if not os.path.exists(rootPath + workingFolder):
-                os.makedirs(rootPath + workingFolder)
+        if(today.isoformat() > workingDir):
+            #Its a new day set a new workingDir and make the folder
+            workingDir = today.isoformat();
+            if not os.path.exists(rootPath + workingDir):
+                print 'creating new workingdir '+ rootPath + workingDir
+                os.makedirs(rootPath + workingDir)
+                print 'Save the image to workingDir'
+                saveImage(url, workingDir)
+                print ' Sleeping' + str(waitTime) + ' seconds'
+                time.sleep(waitTime)
             else:
-                print 'New Days Folder already created....Thats Odd'
+                #This should not happen.
+                print 'New Days Folder already created....Thats Odd\n'
         else:
-            print 'save the image'
-
+            #This case will happen 99% of the time.
+            print 'WorkingFolder is set and no need to create new folder'
+            print 'save the image\n'
+            saveImage(url, workingDir)
+            print 'Sleeping' + str(waitTime) + ' seconds'
+            time.sleep(waitTime);
 
